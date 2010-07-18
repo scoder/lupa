91d4e2fe0a10558e554fdbabd820024acc6d99d1
Stefan Behnel <scoder@users.berlios.de>
2010-07-18 16:57:30 +0200

    code indentation

--- lupa/tests/test.py
+++ lupa/tests/test.py
@@ -221,22 +221,22 @@ function(N)
     local result = ""
     local M, ba, bb, buf = 2/N, 2^(N%8+1)-1, 2^(8-N%8), {}
     for y=0,N-1 do
-      local Ci, b, p = y*M-1, 1, 0
-      for x=0,N-1 do
-        local Cr = x*M-1.5
-        local Zr, Zi, Zrq, Ziq = Cr, Ci, Cr*Cr, Ci*Ci
-        b = b + b
-        for i=1,49 do
-          Zi = Zr*Zi*2 + Ci
-          Zr = Zrq-Ziq + Cr
-          Ziq = Zi*Zi
-          Zrq = Zr*Zr
-          if Zrq+Ziq > 4.0 then b = b + 1; break; end
+        local Ci, b, p = y*M-1, 1, 0
+        for x=0,N-1 do
+            local Cr = x*M-1.5
+            local Zr, Zi, Zrq, Ziq = Cr, Ci, Cr*Cr, Ci*Ci
+            b = b + b
+            for i=1,49 do
+                Zi = Zr*Zi*2 + Ci
+                Zr = Zrq-Ziq + Cr
+                Ziq = Zi*Zi
+                Zrq = Zr*Zr
+                if Zrq+Ziq > 4.0 then b = b + 1; break; end
+            end
+            if b >= 256 then p = p + 1; buf[p] = 511 - b; b = 1; end
         end
-        if b >= 256 then p = p + 1; buf[p] = 511 - b; b = 1; end
-      end
-      if b ~= 1 then p = p + 1; buf[p] = (ba-b)*bb; end
-      result = result .. char(unpack(buf, 1, p))
+        if b ~= 1 then p = p + 1; buf[p] = (ba-b)*bb; end
+        result = result .. char(unpack(buf, 1, p))
     end
     return result
 end
