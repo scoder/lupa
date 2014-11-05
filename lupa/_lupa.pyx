79d1c85e00c12db9dd1f667ac111223306d9358e
Stefan Behnel <stefan_ml@behnel.de>
2014-11-05 19:57:16 +0100

    remove unused import

--- lupa/_lupa.pyx
+++ lupa/_lupa.pyx
@@ -21,7 +21,7 @@ cimport cpython.long
 from cpython.ref cimport PyObject
 from cpython.method cimport (
     PyMethod_Check, PyMethod_GET_SELF, PyMethod_GET_FUNCTION)
-from cpython.version cimport PY_VERSION_HEX, PY_MAJOR_VERSION
+from cpython.version cimport PY_MAJOR_VERSION
 
 from libc.stdint cimport uintptr_t
 
