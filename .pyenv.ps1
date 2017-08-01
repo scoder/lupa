7ce35752d21a04f8589c9410fdb94358e7378430
TitanSnow <sweeto@live.cn>
2017-08-01 18:30:04 +0800

    ready for appveyor

---
+++ .pyenv.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = "Stop"
+
+$safe_paths = @()
+$origin_path = $env:Path
+$env:Path.Split(';') | ForEach-Object{
+    $env:Path = $_
+    $issafe = $true
+    try{
+        Get-Command 'python' | Out-Null
+        $issafe = $false
+    }catch [System.Management.Automation.CommandNotFoundException]{}
+    try{
+        Get-Command 'pip' | Out-Null
+        $issafe = $false
+    }catch [System.Management.Automation.CommandNotFoundException]{}
+    if($issafe){
+        $safe_paths += $_
+    }
+}
+$env:Path = ((@($args[0], (Join-Path $args[0] 'Scripts')) + $safe_paths) -join ';')
+& $args[1]
+$exitcode = $LASTEXITCODE
+$env:Path = $origin_path
+Exit $exitcode
