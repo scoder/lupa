6ac868c83dc3d6ad0733e5155c97c23414b06b06
Stefan Behnel <stefan_ml@behnel.de>
2018-08-06 10:51:24 +0200

    Exclude platform specific doctests on appveyor.

--- lupa/tests/__init__.py
+++ lupa/tests/__init__.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 import unittest
 import doctest
 import os
+import os.path as os_path
+import sys
 
 import lupa
 
@@ -17,7 +19,22 @@ def suite():
 
     suite = unittest.defaultTestLoader.loadTestsFromNames(tests)
     suite.addTest(doctest.DocTestSuite(lupa._lupa))
-    suite.addTest(doctest.DocFileSuite('../../README.rst'))
+
+    # Long version of
+    # suite.addTest(doctest.DocFileSuite('../../README.rst'))
+    # to remove some platform specific tests.
+    readme_filename = 'README.rst'
+    readme_file = os_path.join(os_path.dirname(__file__), '..', '..', readme_filename)
+    with open(readme_file) as f:
+        readme = f.read()
+    if sys.platform != 'linux2':
+        # Exclude last section, which is Linux specific.
+        readme = readme.split('Importing Lua binary modules\n----------------------------\n', 1)[0]
+
+    parser = doctest.DocTestParser()
+    test = parser.get_doctest(readme, {'__file__': readme_file}, 'README.rst', readme_file, 0)
+    suite.addTest(doctest.DocFileCase(test))
+
     return suite
 
 
