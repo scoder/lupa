f112b9b6ba48622a71c5fd50c1f34e43fdec9e46
guidanoli <guidanoli@hotmail.com>
2021-04-04 20:03:54 -0300

    Added tests and added argument checks in python.set_overflow_handler

--- lupa/_lupa.pyx
+++ lupa/_lupa.pyx
@@ -1870,10 +1870,11 @@ cdef int py_iter_next_with_gil(lua_State* L, py_object* py_iter) with gil:
 # overflow handler setter
 
 cdef int py_set_overflow_handler(lua_State* L) nogil:
-    lua.lua_settop(L, 1)                         # hdl
-    lua.lua_pushlstring(L, LUPAOFH, LUPAOFH_LEN) # hdl name
-    lua.lua_pushvalue(L, 1)                      # hdl name hdl
-    lua.lua_rawset(L, lua.LUA_REGISTRYINDEX)     # hdl
+    if not lua.lua_isnil(L, 1) and not lua.lua_isfunction(L, 1):
+        return lua.luaL_argerror(L, 1, "expected nil or function")
+    lua.lua_pushlstring(L, LUPAOFH, LUPAOFH_LEN) # hdl ... name
+    lua.lua_pushvalue(L, 1)                      # hdl ... name hdl
+    lua.lua_rawset(L, lua.LUA_REGISTRYINDEX)     # hdl ...
     return 0
 
 # 'python' module functions in Lua
