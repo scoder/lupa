6a9d891b6cbe0bf1cf8f6042b6fcc4c95a5a5d6a
Leo Developer <leodeveloper@protonmail.com>
2022-09-17 21:39:23 +0200

    fix None as key

--- lupa/_lupa.pyx
+++ lupa/_lupa.pyx
@@ -430,6 +430,8 @@ cdef class LuaRuntime:
             for obj in args:
                 if isinstance(obj, dict):
                     for key, value in obj.iteritems():
+                        if key is None:
+                            raise ValueError("can't use None/nil as key")
                         py_to_lua(self, L, key)
                         py_to_lua(self, L, value)
                         lua.lua_rawset(L, -3)
@@ -446,6 +448,8 @@ cdef class LuaRuntime:
 
                 elif isinstance(obj, Mapping):
                     for key in obj:
+                        if key is None:
+                            raise ValueError("can't use None/nil as key")
                         value = obj[key]
                         py_to_lua(self, L, key)
                         py_to_lua(self, L, value)
