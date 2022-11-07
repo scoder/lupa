bf3113d6d12891963ac606fbe80f4384963890cc
Stefan Behnel <stefan_ml@behnel.de>
2022-11-07 12:31:29 +0100

    Update GH-Action versions and include Py3.11 in build.

--- setup.py
+++ setup.py
@@ -467,6 +467,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Other Scripting Engines',
         'Operating System :: OS Independent',
         'Topic :: Software Development',
