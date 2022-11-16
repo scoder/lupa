a1383f3dade5472a5a72133b7a7474081130bf93
Tom Mason <wheybags@wheybags.com>
2022-11-16 10:49:13 +0100

    Set CFLAGS for Lua build if users don't have it. (GH-225)

--- setup.py
+++ setup.py
@@ -225,6 +225,7 @@ def use_bundled_luajit(path, macros):
     print('Using bundled LuaJIT in %s' % libname)
     print('Building LuaJIT for %r in %s' % (platform, libname))
 
+    build_env = dict(os.environ)
     src_dir = os.path.join(path, "src")
     if platform.startswith('win'):
         build_script = [os.path.join(src_dir, "msvcbuild.bat"), "static"]
@@ -233,7 +234,13 @@ def use_bundled_luajit(path, macros):
         build_script = ["make",  "libluajit.a"]
         lib_file = "libluajit.a"
 
-    output = subprocess.check_output(build_script, cwd=src_dir)
+        if 'CFLAGS' in build_env:
+            if "-fPIC" not in build_env['CFLAGS']:
+                build_env['CFLAGS'] += " -fPIC"
+        else:
+            build_env['CFLAGS'] = "-fPIC"
+
+    output = subprocess.check_output(build_script, cwd=src_dir, env=build_env)
     if lib_file.encode("ascii") not in output:
         print("Building LuaJIT did not report success:")
         print(output.decode().strip())
