34a43bb9c01d53be9e10fd907680b9fd160a9f79
Stefan Behnel <stefan_ml@behnel.de>
2023-10-04 09:09:30 +0200

    Add official support for Python 3.12.

--- setup.py
+++ setup.py
@@ -490,6 +490,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Lua',
         'Programming Language :: Other Scripting Engines',
         'Operating System :: OS Independent',
